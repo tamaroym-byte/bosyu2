4a3b84e5949a0cfde98a4dc0f441d7f6485ba9cc
tamaroym-byte <tamaro.ym@gmail.com>
2026-05-20 14:51:42 +0900

    Create xp.js

---
+++ commands/xp.js
@@ -0,0 +1,25 @@
+const {
+  SlashCommandBuilder
+} = require('discord.js');
+
+const db = require('../db/database');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('xp')
+    .setDescription('XP確認'),
+
+  async execute(interaction) {
+    const data = db.prepare(`
+      SELECT * FROM user_xp
+      WHERE guild_id=? AND user_id=?
+    `).get(interaction.guild.id, interaction.user.id);
+
+    const xp = data?.xp || 0;
+
+    await interaction.reply({
+      content: `あなたのXP: ${xp}`,
+      ephemeral: true
+    });
+  }
+};
