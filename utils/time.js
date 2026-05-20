f40a6aca58bf6686e858347a0fc1c4ac7f84a6d1
tamaroym-byte <tamaro.ym@gmail.com>
2026-05-20 14:57:38 +0900

    Create time.js

---
+++ utils/time.js
@@ -0,0 +1,22 @@
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const JST = 'Asia/Tokyo';
+
+function nowJST() {
+  return dayjs().tz(JST);
+}
+
+function parseJST(datetime) {
+  return dayjs.tz(datetime, JST);
+}
+
+module.exports = {
+  nowJST,
+  parseJST,
+  JST
+};
