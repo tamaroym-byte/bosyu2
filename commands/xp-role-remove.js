efce1ea00ca9c592d8a67cf9238a6c4c253524a8
tamaroym-byte <tamaro.ym@gmail.com>
2026-05-20 14:53:20 +0900

    Create xp-role-remove.js

---
+++ commands/xp-role-remove.js
@@ -0,0 +1,35 @@
+const {
+  SlashCommandBuilder,
+  PermissionFlagsBits
+} = require('discord.js');
+
+const db = require('../db/database');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('xp-role-remove')
+    .setDescription('XPロール削除')
+    .addRoleOption(option =>
+      option
+        .setName('role')
+        .setDescription('ロール')
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(
+      PermissionFlagsBits.Administrator
+    ),
+
+  async execute(interaction) {
+    const role = interaction.options.getRole('role');
+
+    db.prepare(`
+      DELETE FROM xp_roles
+      WHERE guild_id=? AND role_id=?
+    `).run(interaction.guild.id, role.id);
+
+    await interaction.reply({
+      content: 'XPロール削除完了',
+      ephemeral: true
+    });
+  }
+};
