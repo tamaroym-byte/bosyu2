c022805ae28cb83212ed7300a0b931eb4c2a35ce
tamaroym-byte <tamaro.ym@gmail.com>
2026-05-20 14:46:47 +0900

    Initialize Discord bot with command and event handling

---
+++ index.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const path = require('path');
+
+const {
+  Client,
+  GatewayIntentBits,
+  Collection
+} = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.commands = new Collection();
+
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+const eventFiles = fs
+  .readdirSync('./events')
+  .filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+require('./jobs/autoCloseJob')(client);
+
+client.login(process.env.DISCORD_TOKEN);
