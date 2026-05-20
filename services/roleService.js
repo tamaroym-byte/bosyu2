aa6653f4243af1e4db977f6686953a49925bfc0d
tamaroym-byte <tamaro.ym@gmail.com>
2026-05-20 14:54:40 +0900

    Create roleService.js

---
+++ services/roleService.js
@@ -0,0 +1,26 @@
+const db = require('../db/database');
+
+async function checkAndGrantRoles(member, totalXP) {
+  const roles = db.prepare(`
+    SELECT * FROM xp_roles
+    WHERE guild_id=?
+  `).all(member.guild.id);
+
+  for (const roleData of roles) {
+    if (totalXP >= roleData.required_xp) {
+      const role = member.guild.roles.cache.get(
+        roleData.role_id
+      );
+
+      if (!role) continue;
+
+      if (!member.roles.cache.has(role.id)) {
+        await member.roles.add(role);
+      }
+    }
+  }
+}
+
+module.exports = {
+  checkAndGrantRoles
+};
