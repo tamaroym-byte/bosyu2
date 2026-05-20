5a172ba485e2e684b8bd5e9988eaa3a0856f7bbf
tamaroym-byte <tamaro.ym@gmail.com>
2026-05-20 14:53:58 +0900

    Create xp-add.js

---
+++ commands/xp-add.js
@@ -0,0 +1,43 @@
+const {
+  SlashCommandBuilder,
+  PermissionFlagsBits
+} = require('discord.js');
+
+const xpService = require('../services/xpService');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('xp-add')
+    .setDescription('XP付与')
+    .addUserOption(option =>
+      option
+        .setName('user')
+        .setDescription('対象ユーザー')
+        .setRequired(true)
+    )
+    .addIntegerOption(option =>
+      option
+        .setName('amount')
+        .setDescription('XP量')
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(
+      PermissionFlagsBits.Administrator
+    ),
+
+  async execute(interaction) {
+    const user = interaction.options.getUser('user');
+    const amount = interaction.options.getInteger('amount');
+
+    const total = xpService.addXP(
+      interaction.guild.id,
+      user.id,
+      amount
+    );
+
+    await interaction.reply({
+      content: `${user.username} に ${amount}XP付与 (合計:${total})`,
+      ephemeral: true
+    });
+  }
+};
