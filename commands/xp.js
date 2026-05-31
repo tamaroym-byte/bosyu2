8fc60d805247d0d4652a3e51a82e5e60715afc26
tamaroym-byte <tamaro.ym@gmail.com>
2026-05-31 17:26:39 +0900

    Refactor xp command for improved readability

--- commands/xp.js
+++ commands/xp.js
@@ -2,23 +2,37 @@ const {
   SlashCommandBuilder
 } = require('discord.js');
 
-const db = require('../db/database');
+const db =
+  require('../db/database');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('xp')
-    .setDescription('XP確認'),
+
+  data:
+    new SlashCommandBuilder()
+      .setName('xp')
+      .setDescription(
+        'XP確認'
+      ),
 
   async execute(interaction) {
-    const data = db.prepare(`
-      SELECT * FROM user_xp
-      WHERE guild_id=? AND user_id=?
-    `).get(interaction.guild.id, interaction.user.id);
 
-    const xp = data?.xp || 0;
+    const data =
+      db.prepare(`
+        SELECT *
+        FROM user_xp
+        WHERE guild_id=?
+        AND user_id=?
+      `).get(
+        interaction.guild.id,
+        interaction.user.id
+      );
+
+    const xp =
+      data?.xp || 0;
 
     await interaction.reply({
-      content: `あなたのXP: ${xp}`,
+      content:
+        `あなたのXP: ${xp}`,
       ephemeral: true
     });
   }
