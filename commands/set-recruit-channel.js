e4a39204d6cd696e090637cb54a8514a571bb45a
tamaroym-byte <tamaro.ym@gmail.com>
2026-05-31 17:27:05 +0900

    Refactor set-recruit-channel command structure

--- commands/set-recruit-channel.js
+++ commands/set-recruit-channel.js
@@ -3,34 +3,80 @@ const {
   PermissionFlagsBits
 } = require('discord.js');
 
-const db = require('../db/database');
+const db =
+  require('../db/database');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('set-recruit-channel')
-    .setDescription('募集チャンネル設定')
-    .addChannelOption(option =>
-      option
-        .setName('channel')
-        .setDescription('チャンネル')
-        .setRequired(true)
-    )
-    .setDefaultMemberPermissions(
-      PermissionFlagsBits.Administrator
-    ),
+
+  data:
+    new SlashCommandBuilder()
+
+      .setName(
+        'set-recruit-channel'
+      )
+
+      .setDescription(
+        '募集チャンネル設定'
+      )
+
+      .addChannelOption(
+        option =>
+          option
+            .setName('channel')
+            .setDescription(
+              '募集投稿チャンネル'
+            )
+            .setRequired(true)
+      )
+
+      .setDefaultMemberPermissions(
+        PermissionFlagsBits.Administrator
+      ),
 
   async execute(interaction) {
-    const channel = interaction.options.getChannel('channel');
 
-    db.prepare(`
-      INSERT OR REPLACE INTO guild_settings (
-        guild_id,
-        recruit_channel_id
-      ) VALUES (?, ?)
-    `).run(interaction.guild.id, channel.id);
+    const channel =
+      interaction.options.getChannel(
+        'channel'
+      );
+
+    const exists =
+      db.prepare(`
+        SELECT *
+        FROM guild_settings
+        WHERE guild_id=?
+      `).get(
+        interaction.guild.id
+      );
+
+    if (exists) {
+
+      db.prepare(`
+        UPDATE guild_settings
+        SET recruit_channel_id=?
+        WHERE guild_id=?
+      `).run(
+        channel.id,
+        interaction.guild.id
+      );
+
+    } else {
+
+      db.prepare(`
+        INSERT INTO guild_settings (
+          guild_id,
+          recruit_channel_id
+        )
+        VALUES (?, ?)
+      `).run(
+        interaction.guild.id,
+        channel.id
+      );
+    }
 
     await interaction.reply({
-      content: '募集チャンネル設定完了',
+      content:
+        '募集チャンネル設定完了',
       ephemeral: true
     });
   }
