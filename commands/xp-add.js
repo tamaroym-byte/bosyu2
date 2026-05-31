5690d88d28260be63c0486da4504cc2190fbd677
tamaroym-byte <tamaro.ym@gmail.com>
2026-05-31 17:29:13 +0900

    Update xp-add.js

--- commands/xp-add.js
+++ commands/xp-add.js
@@ -3,40 +3,64 @@ const {
   PermissionFlagsBits
 } = require('discord.js');
 
-const xpService = require('../services/xpService');
+const xpService =
+  require('../services/xpService');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('xp-add')
-    .setDescription('XP付与')
-    .addUserOption(option =>
-      option
-        .setName('user')
-        .setDescription('対象ユーザー')
-        .setRequired(true)
-    )
-    .addIntegerOption(option =>
-      option
-        .setName('amount')
-        .setDescription('XP量')
-        .setRequired(true)
-    )
-    .setDefaultMemberPermissions(
-      PermissionFlagsBits.Administrator
-    ),
+
+  data:
+    new SlashCommandBuilder()
+
+      .setName('xp-add')
+
+      .setDescription(
+        'XP付与'
+      )
+
+      .addUserOption(option =>
+        option
+          .setName('user')
+          .setDescription(
+            '対象ユーザー'
+          )
+          .setRequired(true)
+      )
+
+      .addIntegerOption(option =>
+        option
+          .setName('amount')
+          .setDescription(
+            'XP量'
+          )
+          .setRequired(true)
+      )
+
+      .setDefaultMemberPermissions(
+        PermissionFlagsBits.Administrator
+      ),
 
   async execute(interaction) {
-    const user = interaction.options.getUser('user');
-    const amount = interaction.options.getInteger('amount');
 
-    const total = xpService.addXP(
-      interaction.guild.id,
-      user.id,
-      amount
-    );
+    const user =
+      interaction.options.getUser(
+        'user'
+      );
+
+    const amount =
+      interaction.options.getInteger(
+        'amount'
+      );
+
+    const total =
+      xpService.addXP(
+        interaction.guild.id,
+        user.id,
+        amount
+      );
 
     await interaction.reply({
-      content: `${user.username} に ${amount}XP付与 (合計:${total})`,
+      content:
+        `${user.username} に ${amount}XP付与 (合計:${total})`,
       ephemeral: true
     });
   }
