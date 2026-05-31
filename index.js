e8778ad12c7bf15a89c864e14acac9725bd4f31e
tamaroym-byte <tamaro.ym@gmail.com>
2026-05-31 16:29:48 +0900

    Refactor code formatting for better readability

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const fs = require('fs');
-const path = require('path');
 
 const {
   Client,
@@ -24,8 +23,14 @@ const commandFiles = fs
   .filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.data.name, command);
+
+  const command =
+    require(`./commands/${file}`);
+
+  client.commands.set(
+    command.data.name,
+    command
+  );
 }
 
 const eventFiles = fs
@@ -33,12 +38,25 @@ const eventFiles = fs
   .filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
-  const event = require(`./events/${file}`);
+
+  const event =
+    require(`./events/${file}`);
 
   if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args, client));
+
+    client.once(
+      event.name,
+      (...args) =>
+        event.execute(...args, client)
+    );
+
   } else {
-    client.on(event.name, (...args) => event.execute(...args, client));
+
+    client.on(
+      event.name,
+      (...args) =>
+        event.execute(...args, client)
+    );
   }
 }
 
