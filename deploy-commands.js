20c74a26c8c349d728926b4cf3db0b2bd58d1786
tamaroym-byte <tamaro.ym@gmail.com>
2026-05-31 16:30:29 +0900

    Improve formatting and add error handling for command deployment
    Refactor code for better readability and error handling.

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 
 const fs = require('fs');
-const { REST, Routes } = require('discord.js');
+
+const {
+  REST,
+  Routes
+} = require('discord.js');
 
 const commands = [];
 
@@ -10,21 +14,41 @@ const commandFiles = fs
   .filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  commands.push(command.data.toJSON());
+
+  const command =
+    require(`./commands/${file}`);
+
+  commands.push(
+    command.data.toJSON()
+  );
 }
 
-const rest = new REST({ version: '10' })
-  .setToken(process.env.DISCORD_TOKEN);
+const rest = new REST({
+  version: '10'
+}).setToken(
+  process.env.DISCORD_TOKEN
+);
 
 (async () => {
-  await rest.put(
-    Routes.applicationGuildCommands(
-      process.env.CLIENT_ID,
-      process.env.GUILD_ID
-    ),
-    { body: commands }
-  );
 
-  console.log('Commands deployed');
+  try {
+
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      {
+        body: commands
+      }
+    );
+
+    console.log(
+      'Commands deployed'
+    );
+
+  } catch (err) {
+
+    console.error(err);
+  }
 })();
