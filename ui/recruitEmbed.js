6c1dfbf4ef717341ac8e9b282c3d3d1bec36cf30
tamaroym-byte <tamaro.ym@gmail.com>
2026-05-31 16:32:28 +0900

    Update recruitEmbed.js

--- ui/recruitEmbed.js
+++ ui/recruitEmbed.js
@@ -1,20 +1,52 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder } =
+  require('discord.js');
 
-function buildEmbed(recruit, participants) {
-  const remain = recruit.max_players - participants.length;
+const dayjs =
+  require('dayjs');
+
+const utc =
+  require('dayjs/plugin/utc');
+
+const timezone =
+  require('dayjs/plugin/timezone');
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+function buildEmbed(
+  recruit,
+  participants
+) {
+
+  const remain =
+    recruit.max_players -
+    participants.length;
 
   let color = 0x3498db;
 
-  if (recruit.status === 'PAUSED') color = 0xf39c12;
-  else if (recruit.status === 'CLOSED') color = 0x7f8c8d;
-  else if (remain === 0) color = 0x2ecc71;
-  else if (remain === 1) color = 0xf1c40f;
+  if (recruit.status === 'PAUSED') {
+    color = 0xf39c12;
+  }
+  else if (
+    recruit.status === 'CLOSED'
+  ) {
+    color = 0x7f8c8d;
+  }
+  else if (remain === 0) {
+    color = 0x2ecc71;
+  }
+  else if (remain === 1) {
+    color = 0xf1c40f;
+  }
 
-  let title = `${recruit.game_name} 参加者募集`;
+  let title =
+    `${recruit.game_name} 参加者募集`;
 
   switch (recruit.status) {
+
     case 'OPEN':
-      title += ` あと${remain}名募集中！`;
+      title +=
+        ` あと${remain}名募集中！`;
       break;
 
     case 'FULL':
@@ -26,21 +58,42 @@ function buildEmbed(recruit, participants) {
       break;
 
     case 'CLOSED':
-      title += ' は終了しました';
+      title +=
+        ' は終了しました';
       break;
   }
+
+  const startText =
+    dayjs(recruit.start_time)
+      .tz('Asia/Tokyo')
+      .format('MM/DD HH:mm');
+
+  const expireText =
+    dayjs(recruit.expires_at)
+      .tz('Asia/Tokyo')
+      .format('MM/DD HH:mm');
 
   return new EmbedBuilder()
     .setTitle(title)
     .setColor(color)
     .setDescription(
-      `予定開始時刻\n${recruit.start_time}\n\n` +
-      `募集状況\n${participants.length} / ${recruit.max_players}\n\n` +
+
+      `予定開始時刻\n${startText}\n\n` +
+
+      `募集状況\n` +
+      `${participants.length}` +
+      ` / ${recruit.max_players}\n\n` +
+
       `募集主: <@${recruit.host_id}>\n\n` +
+
       `募集ID: ${recruit.id}\n` +
-      `有効期限: ${recruit.expires_at}\n\n` +
-      `備考\n${recruit.note || 'なし'}`
+
+      `有効期限: ${expireText}\n\n` +
+
+      `備考\n` +
+      `${recruit.note || 'なし'}`
     );
 }
 
-module.exports = buildEmbed;
+module.exports =
+  buildEmbed;
